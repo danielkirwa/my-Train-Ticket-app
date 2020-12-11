ac60ff042863a98c756c51e0aa49f83fc971bea3
danielkirwa <kirwadan03@gmail.com>
2020-12-11 18:47:56 +0300

    select add to form

--- scripts/fastbook.js
+++ scripts/fastbook.js
@@ -103,12 +103,9 @@ var btndatetimevalidate = document.getElementById('depaturetimeoption');
 
  let btncallpopup = document.getElementById('booknowhighclass');
  btncallpopup.addEventListener('click', () =>{
- 	let popupform = document.querySelector('.popupbookingform');
- 	let hidemainform = document.querySelector('.main-context');
  	// display the form and populate it with input fieds
- 	popupform.style.display = "block";
- 	hidemainform.style.display = "none";
- 	createticketform();
+ 	document.querySelector('.formcaption').innerHTML = "High Class Ticket";
+ 	document.querySelector('.ticketcategory').selectedIndex = "1";
  	//document.documentElement.scrollTop = 0;
  	//window.scrollTo({top: 0, behavior: 'smooth'});
  	scrollToTop();
@@ -119,83 +116,15 @@ var btndatetimevalidate = document.getElementById('depaturetimeoption');
  // popup booking form for economy
   let btncallpopup2 = document.getElementById('booknoweconomy');
  btncallpopup2.addEventListener('click', () =>{
- 	let popupform = document.querySelector('.popupbookingform');
- 	let hidemainform = document.querySelector('.main-context');
  	// display the form and populate it with input fieds
- 	popupform.style.display = "block";
- 	hidemainform.style.display = "none";
- 	createticketform();
- 	document.querySelector('.ticketlabel').innerHTML = "Economy Ticket";
+ 	document.querySelector('.formcaption').innerHTML = "Economy Ticket";
+ 	document.querySelector('.ticketcategory').selectedIndex = "0";
  	//document.documentElement.scrollTop = 0;
  	//window.scrollTo({top: 0, behavior: 'smooth'});
  	scrollToTop();
    
  })
 
- function createticketform() {
- 	// body...
- 	let popupform = document.querySelector('.popupbookingform');
- 	popupform.innerHTML += `
-     	<center>
-     	<h2 style="color:crimson;" class="ticketlabel"> High-class Ticket</h2>
-	<!-- <form> -->
-		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<label>From :</label>
-		<select id="depatureoption">
-			<option value="0">Depature</option>
-			<option value="1">NAIROBI</option>
-			<option value="2">MOMBASA</option>
-			<option value="3">KISUMU</option>
-			<option value="4">NAKURU</option>
-		</select>
-		<br><br>
-		<label>Destination :</label>
-		<select id="destinstionoption">
-			<option value="0">Arrival</option>
-			<option value="1">NAIROBI</option>
-			<option value="2">MOMBASA</option>
-			<option value="3">KISUMU</option>
-			<option value="4">NAKURU</option>
-		</select><br><br><br>
-		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-		<label>Date :</label>
-		<input type="date" name="" id="highclassdepaturedate"><br><br>
-		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-		<label>Time :</label>
-		<select id="depaturetimeoption">
-			<option  value="4">4:00 AM</option>
-			<option  value="5">5:00 AM</option>
-			<option  value="5">6:00 AM</option>
-			<option  value="7">7:00 AM</option>
-			<option  value="8">8:00 AM</option>
-			<option  value="9">9:00 AM</option>
-			<option  value="10">10:00 AM</option>
-			<option  value="11">11:00 AM</option>
-			<option  value="12">12:00 AM</option>
-			<option  value="13">1:00 PM</option>
-			<option  value="14">2:00 PM</option>
-			<option  value="15">3:00 PM</option>
-			<option  value="16">4:00 PM</option>
-			<option  value="17">5:00 PM</option>
-			<option  value="18">6:00 PM</option>
-			<option  value="19">7:00 PM</option>
-			<option  value="20">8:00 PM</option>
-			<option  value="21">9:00 PM</option>
-			<option  value="22">10:00 PM</option>
-			<option  value="23">11:00 PM</option>
-		</select><br><br>
-			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<label>Class :</label>
-		<select>
-			<option>High-class</option>
-			
-		</select><br><br>
-		<button id="btnpopuphomebooknow">Book Now</button>
-	<!-- </form> -->
-
-</center>
-
-     `
-
- }
  // for slow motion scroll top 
  const scrollToTop = () => {
   const c = document.documentElement.scrollTop || document.body.scrollTop;
