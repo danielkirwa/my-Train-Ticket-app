8bdda03bf00dd936e10917cc7980a5dbdf65cb7a
danielkirwa <kirwadan03@gmail.com>
2020-12-21 22:07:36 +0300

    modified file added incomplete table sort

--- scripts/departure.js
+++ scripts/departure.js
@@ -1,16 +1,22 @@
+// get date 
+
+let date = new Date();
+let today = date.toISOString().slice(0, 10);
+
+
 var departurearraylist = [
-	{'Name' : 'Spark train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1700hrs', 'Date':'20-12-2020'},
-	{'Name' : 'Bullet train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1800hrs', 'Date':'20-12-2020'},
-	{'Name' : 'King train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1900hrs', 'Date':'20-12-2020'},
-	{'Name' : 'Spring train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1700hrs', 'Date':'20-12-2020'},
-	{'Name' : 'Cue train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1700hrs', 'Date':'20-12-2020'},
-	{'Name' : 'Bull train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1800hrs', 'Date':'20-12-2020'},
-	{'Name' : 'Spinner train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1900hrs', 'Date':'20-12-2020'},
-	{'Name' : 'Wave train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1700hrs', 'Date':'20-12-2020'},
-	{'Name' : 'Knight train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1700hrs', 'Date':'20-12-2020'},
-	{'Name' : 'Mini train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1800hrs', 'Date':'20-12-2020'},
-	{'Name' : 'Zue train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1900hrs', 'Date':'20-12-2020'},
-	{'Name' : 'Wauq train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1700hrs', 'Date':'20-12-2020'}
+	{'Name' : 'Spark train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '0400hrs', 'Date':today},
+	{'Name' : 'Bullet train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '0600hrs', 'Date':today},
+	{'Name' : 'King train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '0700hrs', 'Date':today},
+	{'Name' : 'Spring train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '0800hrs', 'Date':today},
+	{'Name' : 'Cue train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1000hrs', 'Date':today},
+	{'Name' : 'Bull train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1100rs', 'Date':today},
+	{'Name' : 'Spinner train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1200hrs', 'Date':today},
+	{'Name' : 'Wave train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1400hrs', 'Date':today},
+	{'Name' : 'Knight train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1500hrs', 'Date':today},
+	{'Name' : 'Mini train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1700hrs', 'Date':today},
+	{'Name' : 'Zue train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1800hrs', 'Date':today},
+	{'Name' : 'Wzauq train','Departure' : 'nairobi','Destination' : 'mombasa','Time' : '1900hrs', 'Date':today}
 
 ]
 
@@ -113,3 +119,28 @@ function subterminals(data) {
 		table.innerHTML += row;
 	}
 }
+
+// sort out terminal table to display non departed train
+function looprows() {
+	// body...
+
+var datadisplay = document.getElementById('readrows');
+datadisplay.innerHTML = "";
+var myrows  = document.getElementById('dep');
+   for(i = 0; i = myrows.rows.length; i ++){
+
+   	// get current row cell 
+
+   	let myrowcell = myrows.rows.item(i).cells;
+   	// read cell value in a loop 
+   	for(r  = 0 ; r  = myrowcell.length; r ++){
+   		datadisplay.innerHTML = datadisplay.innerHTML + '' + myrowcell.item(i).innerHTML;
+
+   	}
+
+   	datadisplay.innerHTML = datadisplay.innerHTML + '<br>';
+
+   }
+   }
+
+ 
