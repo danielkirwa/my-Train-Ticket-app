bd4c3c971e2dd1fe6fc2c1b3738a90be77626481
danielkirwa <kirwadan03@gmail.com>
2020-12-29 22:30:38 +0300

    modified file

--- scripts/bookings.js
+++ scripts/bookings.js
@@ -1,10 +1,23 @@
  let btncancel = document.querySelector('.btncancelbook');
  // loop through cancel button
 
- for (let i = 0 ; i < btncancel.length ;  i++) {
+ btncancel.addEventListener('click' , () =>{
+
+ 	var confirmcancel = confirm("Sure To Cancel Book");
+ 	if(confirmcancel){
+ 		document.querySelector('.holder').innerHTML = "";
+ 	}else{
+ 		alert("Opparaion canceled")
+ 	}
+
+ 		
+ 		
+ 	})
+
+ /*for (let i = 0 ; i < btncancel.length ;  i++) {
  	
- 	btncancel[i].addEventListerner('click' , () =>{
+ 	btncancel[i].addEventListener('click' , () =>{
  		document.querySelector('.holder').innerHTML = "";
  		alert('helloe');
  	})
- }
+ }*/
