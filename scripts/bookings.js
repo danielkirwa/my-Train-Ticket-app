cf657dbebafe0a5efc077c9ae0f0687f09cf9997
danielkiwa <kirwadan03@gmail.com>
2021-08-25 10:52:24 +0300

    ticket booking implementation

--- scripts/bookings.js
+++ scripts/bookings.js
@@ -8,7 +8,8 @@ var ticketDestinationtTerminal = document.getElementById('ticket-destination-ter
 var ticketDate = document.getElementById('ticket-date');
 var ticketTime = document.getElementById('ticket-time');
 var processingTickets = document.querySelector('.processing-tickets');
-
+var btnsubmitbook = document.querySelector('.submit-book');
+var bookedSeatNumbers = "";
  // populate ticket
 
  function populateticket() {
@@ -18,9 +19,83 @@ var processingTickets = document.querySelector('.processing-tickets');
  		 let ticketHolder = document.querySelector('.holder');
 
  		 if(ticketDetails){
+      var mytime = ticketDetails.TicketTime;
+      mytime = parseInt(mytime)
+      var myNewtime;
+     
+      switch (mytime){
+        case 4: 
+        myNewtime ="4:00 AM";
+        break; 
+         case 5: 
+         myNewtime ="5:00 AM";
+        break; 
+         case 6: 
+         myNewtime ="6:00 AM";
+        break; 
+         case 7: 
+         myNewtime ="7:00 AM";
+        break; 
+         case 8: 
+         myNewtime ="8:00 AM";
+        break; 
+         case 9: 
+         myNewtime ="9:00 AM";
+        break; 
+        case 10: 
+         myNewtime ="10:00 AM";
+        break;
+        case 11: 
+         myNewtime ="11:00 AM";
+        break;
+        case 12: 
+         myNewtime ="12:00 PM";
+        break;
+        case 13: 
+         myNewtime ="1:00 AM";
+        break;
+        case 14: 
+         myNewtime ="2:00 PM";
+        break;
+          case 15: 
+         myNewtime ="3:00 PM";
+        break;
+          case 16: 
+         myNewtime ="4:00 PM";
+        break;
+          case 17: 
+         myNewtime ="5:00 PM";
+        break;
+          case 18: 
+         myNewtime ="6:00 PM";
+        break;
+          case 19: 
+         myNewtime ="7:00 PM";
+        break;
+          case 20: 
+         myNewtime ="8:00 PM";
+        break;
+          case 21: 
+         myNewtime ="9:00 PM";
+        break;
+          case 22: 
+         myNewtime ="10:00 PM";
+        break;
+          case 23: 
+         myNewtime ="11:00 PM";
+        break;
+        default:
+        myNewtime = "null";
+      }
+
  		processingTickets.innerHTML = "Booking in progress"
  		 processingTickets.style.color = "green";
- 		 	 ticketCategory.innerHTML = ticketDetails.TicketCategory;
+ 		 	 ticketNumber.innerHTML = "NULL";
+       ticketCategory.innerHTML = ticketDetails.TicketCategory;
+       ticketDepatureTerminal.innerHTML = ticketDetails.TicketDepature;
+       ticketDestinationtTerminal.innerHTML = ticketDetails.TicketDestination;
+       ticketDate.innerHTML = ticketDetails.TicketDate;
+       ticketTime.innerHTML = myNewtime;
 
  		 
  		 }else{
@@ -40,3 +115,23 @@ var processingTickets = document.querySelector('.processing-tickets');
    	document.querySelector('.mytickes').innerHTML = ticketNumber;
    }());
  populateticket();
+// seat selectors 
+
+let seat1 = document.getElementById("seat-1");
+seat1.addEventListener('click' , () =>{
+  document.querySelector('.seat-1').className = "booked-seat";
+  bookedSeatNumbers = bookedSeatNumbers + "1,";
+  writeSeatNumber();
+})
+let seat2 = document.getElementById("seat-2");
+seat2.addEventListener('click' , () =>{
+  document.querySelector('.seat-2').className = "booked-seat";
+  bookedSeatNumbers = bookedSeatNumbers + "2,";
+  writeSeatNumber();
+})
+
+function writeSeatNumber() {
+  // body...
+  let bookedNumbers = document.getElementById('bookedNumbers');
+  bookedNumbers.innerHTML = bookedSeatNumbers;
+}
